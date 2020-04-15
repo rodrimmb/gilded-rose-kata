8b6d420b859aaa85216375f51960738b361759a3
rodrimmb <rodrimmb@gmail.com>
2020-04-15 20:10:34 +0200

    Sacadas a constantes la calidad maxima y minima que puede tomar un Item

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -6,6 +6,9 @@ class GildedRose {
     private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
+    private static final int MAX_QUALITY = 50;
+    private static final int MINUMUM_QUALITY = 0;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -16,24 +19,24 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].getName().equals(AGED_BRIE)
                     && !items[i].getName().equals(BACKSTAGE)) {
-                if (items[i].getQuality() > 0) {
+                if (items[i].getQuality() > MINUMUM_QUALITY) {
                     if (!items[i].getName().equals(SULFURAS)) {
                         items[i].decreaseQuality();
                     }
                 }
             } else {
-                if (items[i].getQuality() < 50) {
+                if (items[i].getQuality() < MAX_QUALITY) {
                     items[i].increaseQuality();
 
                     if (items[i].getName().equals(BACKSTAGE)) {
                         if (items[i].getSellIn() < 11) {
-                            if (items[i].getQuality() < 50) {
+                            if (items[i].getQuality() < MAX_QUALITY) {
                                 items[i].increaseQuality();
                             }
                         }
 
                         if (items[i].getSellIn() < 6) {
-                            if (items[i].getQuality() < 50) {
+                            if (items[i].getQuality() < MAX_QUALITY) {
                                 items[i].increaseQuality();
                             }
                         }
@@ -48,7 +51,7 @@ class GildedRose {
             if (items[i].isPastSellInDate()) {
                 if (!items[i].getName().equals(AGED_BRIE)) {
                     if (!items[i].getName().equals(BACKSTAGE)) {
-                        if (items[i].getQuality() > 0) {
+                        if (items[i].getQuality() > MINUMUM_QUALITY) {
                             if (!items[i].getName().equals(SULFURAS)) {
                                 items[i].decreaseQuality();
                             }
@@ -57,7 +60,7 @@ class GildedRose {
                         items[i].dropQualityTo0();
                     }
                 } else {
-                    if (items[i].getQuality() < 50) {
+                    if (items[i].getQuality() < MAX_QUALITY) {
                         items[i].increaseQuality();
                     }
                 }
