ecc311e9d88001c1af1373c6ec250fa61c038e1e
rodrimmb <rodrimmb@gmail.com>
2020-04-15 18:57:07 +0200

    Añadidos test para comprobar que el codigo cumple con lo que esperamos que haga

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,174 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void qualityDegrades() {
+        Item[] items = new Item[] { new Item("foo", 5, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("foo", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(9, app.items[0].quality);
+    }
+
+    @Test
+    void sellDateHasPassedQualityDegradesTwiceFast() {
+        Item[] items = new Item[] { new Item("foo", 0, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("foo", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    void sellDateIsNegativeQualityDegradesTwiceFast() {
+        Item[] items = new Item[] { new Item("foo", -1, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("foo", app.items[0].name);
+        assertEquals(-2, app.items[0].sellIn);
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    void theQualityIsNeverNegative() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("foo", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void agedBrieIncreasesQualityWhenOlder() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 1) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Aged Brie", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(2, app.items[0].quality);
+    }
+
+    @Test
+    void qualityNeverMoreThan50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 50) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Aged Brie", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    void SulfurasNeverDecreasesQuality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 5, 8) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(5, app.items[0].sellIn);
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    void backstagePassesIncreasesQualityWhenLeftMoreThan10Days() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 8) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(14, app.items[0].sellIn);
+        assertEquals(9, app.items[0].quality);
+    }
+
+    @Test
+    void backstagePassesIncreasesQualityBy2WhenLeft10Days() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 8) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(10, app.items[0].quality);
+    }
+
+    @Test
+    void backstagePassesIncreasesQualityBy2WhenLeftLessThan10Days() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 8, 8) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(7, app.items[0].sellIn);
+        assertEquals(10, app.items[0].quality);
+    }
+
+    @Test
+    void backstagePassesIncreasesQualityBy3WhenLeft5Days() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 8) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(11, app.items[0].quality);
+    }
+
+    @Test
+    void backstagePassesIncreasesQualityBy3WhenLeftLessThan5Days() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 3, 8) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(2, app.items[0].sellIn);
+        assertEquals(11, app.items[0].quality);
+    }
+
+    @Test
+    void backstagePassesQualityDrops0AfterConcert() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 8) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void backstagePassesQualityDrops0AfterConcert_minusOne() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", -1, 8) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(-2, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
 }
